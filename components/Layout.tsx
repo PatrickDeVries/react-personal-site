70f8301fa9efd81fdf2f994be68f8ca0eeb1a873
Patrick DeVries <pcdevri@gmail.com>
2021-10-23 23:33:29 -0500

    Eyes tracking

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,6 +5,7 @@ import { MainNavigation } from '.';
 import { useTheme } from './ThemeContext';
 import * as THREE from 'three';
 import useWindowDimensions from './utils/UseWindowDimensions';
+import { mdiCubeOutline } from '@mdi/js';
 
 const HomeDiv = styled.div`
   ${() => {
@@ -34,7 +35,7 @@ const Body = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
-  height: fit-content;
+  height: 100%;
   padding-bottom: 5rem;
   z-index: 5;
 `;
@@ -44,28 +45,63 @@ const Layout = props => {
   const { theme } = useTheme();
   // === THREE.JS CODE START ===
   useEffect(() => {
+    var mouse = new THREE.Vector2(0, 0);
+    document.onmousemove = event => {
+      mouse.x = (event.clientX / dimensions.width) * 2 - 1;
+      mouse.y = -(event.clientY / dimensions.height) * 2 + 1;
+    };
+
+    var loader = new THREE.TextureLoader();
+    var cubes = [];
     var scene = new THREE.Scene();
+    loader.load('/eye2.jpeg', texture => {
+      texture.offset = new THREE.Vector2(0, 0);
+      var geometry = new THREE.SphereGeometry(0.5, 15, 15);
+      geometry.rotateX(Math.PI / 2);
+
+      geometry.rotateY((Math.PI * 3) / 2);
+      // geometry.rotateZ(Math.PI / 2);
+
+      var material = new THREE.MeshBasicMaterial({ map: texture });
+      // var material = new THREE.MeshBasicMaterial({ color: theme.strongHighlight });
+      for (let i = 0; i < 10; i++) {
+        var cube = new THREE.Mesh(geometry, material);
+        cube.userData = {
+          v: {
+            x: (Math.random() % 0.1) - 0.05,
+            y: (Math.random() % 0.1) - 0.05,
+            z: 0,
+          },
+          t: {
+            x: 1,
+            y: 1,
+            z: 1,
+          },
+        };
+        cubes.push(cube);
+        scene.add(cube);
+      }
+    });
+
     var camera = new THREE.PerspectiveCamera(75, dimensions.width / dimensions.height, 0.1, 1000);
     var renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(dimensions.width, dimensions.height);
     renderer.setClearColor(0x000000, 0);
     document.getElementById('canvas').appendChild(renderer.domElement);
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: theme.strongHighlight });
-    var cubes = [];
-    for (let i = 0; i < 10; i++) {
-      var cube = new THREE.Mesh(geometry, material);
-      cube.userData = {
-        v: {
-          x: (Math.random() % 0.1) - 0.05,
-          y: (Math.random() % 0.2) - 0.1,
-          z: (Math.random() % 0.2) - 0.1,
-        },
-      };
-      cubes.push(cube);
-      scene.add(cube);
-    }
+    // var geometry = new THREE.BoxGeometry(1, 1, 1);
+    // var geometry = new THREE.SphereGeometry(0.5, 10, 10);
+
+    // var material = new THREE.MeshBasicMaterial({ map: texture, overdraw: 0.5 });
+    // cubes.forEach(cube => {
+    //   scene.add(cube);
+    // });
+    console.log(scene);
+
     camera.position.z = 5;
+
+    var plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -2);
+    var raycaster = new THREE.Raycaster();
+    var intersectPoint = new THREE.Vector3();
 
     var animate = function () {
       var width = dimensions.width,
@@ -75,14 +111,40 @@ const Layout = props => {
       if (renderer.width !== dimensions.width || renderer.height !== dimensions.height) {
         renderer.setSize(dimensions.width, dimensions.height);
       }
-
+      raycaster.setFromCamera(mouse, camera);
+      raycaster.ray.intersectPlane(plane, intersectPoint);
       requestAnimationFrame(animate);
 
-      cubes.forEach(cube => {
+      cubes.forEach((cube, index) => {
         cube.position.x += cube.userData.v.x;
         cube.position.y += cube.userData.v.y;
-        cube.lookAt(0, 0, 0);
-        // cube.position.z += velocity.z;
+
+        // if (mx && my) {
+        //   cube.userData.t.x += (mx - cube.userData.t.x) * 0.2;
+        //   cube.userData.t.y += (-my - cube.userData.t.y) * 0.2;
+        //   // cube.userData.t.z = camera.position.z;
+        //   // console.log(cube.userData.t);
+        //   // var target = new THREE.Vector3(cube.userData.t.x, cube.userData.t.y, 0);
+        //   // if (index === 1) {
+        //   //   console.log(target);
+        //   // }
+        //   // target.project(camera);
+        //   // if (index === 1) {
+        //   //   console.log(target);
+        //   // }
+        //   // if (target.x) {
+        //   // if (mx - widthHalf >= 0) {
+        //   cube.lookAt(cube.userData.t.x, cube.userData.t.y, 9);
+        // } else {
+        // cube.lookAt(cube.userData.t.x + widthHalf, cube.userData.t.y, 0);
+        // }
+        // }
+        // }
+        // cube.lookAt(0, 0, 0);
+        // if (cube.position.x > 0) {
+        //   cube.lookAt(widthHalf, 0, 0);
+        // }
+        cube.lookAt(intersectPoint.x, intersectPoint.y, intersectPoint.z);
 
         var pos = cube.position.clone();
 
@@ -122,3 +184,4 @@ const Layout = props => {
 };
 
 export default Layout;
+//
