df4bf2521d0e933b52b40e690c6c93daad4a6575
Patrick DeVries <39521214+PatrickDeVries@users.noreply.github.com>
2022-02-12 23:35:26 -0600

    Remove foundry from WorkCard

--- components/workCard/WorkCard.tsx
+++ components/workCard/WorkCard.tsx
@@ -1,120 +1,34 @@
-import { Tag, Text, variants } from '@headstorm/foundry-react-ui'
-import { mdiOpenInNew } from '@mdi/js'
 import Link from 'next/link'
 import React from 'react'
-import styled from 'styled-components'
 import { workItem } from '../../resources/myWork'
-import { DESKTOP, MOBILE, SMALL_MOBILE } from '../../styles/mediaQueries'
-import { useTheme } from '../ThemeContext'
-
-const StyledCard = styled.div`
-  background-color: ${({ theme }) => theme.backgroundHighlight};
-  width: fit-content;
-  align-self: stretch;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  border-radius: 0.5rem;
-  padding: 1rem;
-  ${DESKTOP} {
-    max-width: calc(50% - 1rem);
-    min-height: 40vh;
-  }
-  ${MOBILE} {
-    max-width: 100%;
-  }
-  outline: 1px solid ${({ theme }) => theme.secondary};
-`
-
-const BodySection = styled.div`
-  display: flex;
-  flex-direction: row;
-  ${SMALL_MOBILE} {
-    flex-direction: column;
-    align-items: center;
-  }
-  width: 100%;
-`
-
-const ScalingImg = styled.img`
-  display: block;
-  max-width: 40%;
-  object-fit: cover;
-  padding: 1rem;
-`
-
-const BodyText = styled(Text.Container)`
-  margin: 1rem;
-  text-align: justify;
-  text-justify: inter-word;
-`
-
-const TagSection = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
-  flex-direction: row;
-  width: 100%;
-  margin-left: auto;
-  bottom: 1rem;
-`
-
+import { BodySection, BodyText, HeaderText, ScalingImg, StyledCard, Tag, TagSection } from './style'
 interface WorkCardProps {
   item: workItem
 }
 
 const WorkCard = ({ item }: WorkCardProps) => {
-  const { theme } = useTheme()
   return (
     <StyledCard>
-      {/* <Card */}
-      {/* StyledContainer={StyledCard} */}
-      {/* header={
-        <Link href={item?.page || item?.href} passHref={true}>
-          <a>
-            <Text size="1.25rem" color={theme.primary} iconSuffix={mdiOpenInNew}>
-              {item.header}
-            </Text>
-          </a>
-        </Link>
-      } */}
-      {/* footer={
-        <TagSection>
-          {item.tags.map(tagText => (
-            <Tag key={tagText} variant={variants.outline} color={theme.secondary}>
-              <Text size="1rem">{tagText}</Text>
-            </Tag>
-          ))}
-        </TagSection>
-      }
-    > */}
       <div>
         <Link href={item?.page || item?.href} passHref={true}>
           <a>
-            <Text size="1.25rem" color={theme.primary} iconSuffix={mdiOpenInNew}>
-              {item.header}
-            </Text>
+            <HeaderText>{item.header}</HeaderText>
           </a>
         </Link>
         <Link href={item?.page || item?.href} passHref={true}>
           <a>
             <BodySection>
               <ScalingImg src={item.image} />
-              <Text color={theme.text} StyledContainer={BodyText}>
-                {item.description}
-              </Text>
+              <BodyText>{item.description}</BodyText>
             </BodySection>
           </a>
         </Link>
       </div>
       <TagSection>
         {item.tags.map(tagText => (
-          <Tag key={tagText} variant={variants.outline} color={theme.secondary}>
-            <Text size="1rem">{tagText}</Text>
-          </Tag>
+          <Tag key={tagText}>{tagText}</Tag>
         ))}
       </TagSection>
-      {/* </Card> */}
     </StyledCard>
   )
 }
---
+++ components/workCard/style.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components'
+import { DESKTOP, MOBILE, SMALL_MOBILE } from '../../styles/mediaQueries'
+
+export const StyledCard = styled.div`
+  background-color: ${({ theme }) => theme.backgroundHighlight};
+  width: fit-content;
+  align-self: stretch;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 0.5rem;
+  padding: 1rem;
+  ${DESKTOP} {
+    max-width: calc(50% - 1rem);
+    min-height: 40vh;
+  }
+  ${MOBILE} {
+    max-width: 100%;
+  }
+  border: 1px solid ${({ theme }) => theme.secondary};
+`
+
+export const HeaderText = styled.p`
+  font-size: 1.25rem;
+  color: ${({ theme }) => theme.primary};
+`
+
+export const BodySection = styled.div`
+  display: flex;
+  flex-direction: row;
+  ${SMALL_MOBILE} {
+    flex-direction: column;
+    align-items: center;
+  }
+  width: 100%;
+`
+
+export const ScalingImg = styled.img`
+  display: block;
+  max-width: 40%;
+  object-fit: cover;
+  padding: 1rem;
+`
+
+export const BodyText = styled.p`
+  margin: 1rem;
+  text-align: justify;
+  text-justify: inter-word;
+  color: ${({ theme }) => theme.text};
+`
+
+export const TagSection = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  flex-direction: row;
+  width: 100%;
+  margin-left: auto;
+  bottom: 1rem;
+`
+
+export const Tag = styled.p`
+  color: ${({ theme }) => theme.secondary};
+  border: 1px solid ${({ theme }) => theme.secondary};
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+  margin: 0;
+`
